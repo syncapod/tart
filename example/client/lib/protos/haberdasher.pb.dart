a13a7a013b9e0e6016a11b4c30fdc8c5edf741f8
Sam Schwartz <sam.schwartz96@gmail.com>
2023-07-12 10:53:50 -0500

    update example/client pubspec.yaml
    http: v1.1.0

--- example/client/lib/protos/haberdasher.pb.dart
+++ example/client/lib/protos/haberdasher.pb.dart
@@ -1,9 +1,13 @@
-///
+//
 //  Generated code. Do not modify.
 //  source: haberdasher.proto
 //
 // @dart = 2.12
-// ignore_for_file: annotate_overrides,camel_case_types,constant_identifier_names,directives_ordering,library_prefixes,non_constant_identifier_names,prefer_final_fields,return_of_invalid_type,unnecessary_const,unnecessary_import,unnecessary_this,unused_import,unused_shown_name
+
+// ignore_for_file: annotate_overrides, camel_case_types
+// ignore_for_file: constant_identifier_names, library_prefixes
+// ignore_for_file: non_constant_identifier_names, prefer_final_fields
+// ignore_for_file: unnecessary_import, unnecessary_this, unused_import
 
 import 'dart:async' as $async;
 import 'dart:core' as $core;
@@ -13,39 +17,22 @@ import 'package:protobuf/protobuf.dart' as $pb;
 import 'suit.pb.dart' as $0;
 
 class Size extends $pb.GeneratedMessage {
-  static final $pb.BuilderInfo _i = $pb.BuilderInfo(
-      const $core.bool.fromEnvironment('protobuf.omit_message_names')
-          ? ''
-          : 'Size',
-      package: const $pb.PackageName(
-          const $core.bool.fromEnvironment('protobuf.omit_message_names')
-              ? ''
-              : 'protos'),
-      createEmptyInstance: create)
-    ..a<$core.int>(
-        1,
-        const $core.bool.fromEnvironment('protobuf.omit_field_names')
-            ? ''
-            : 'inches',
-        $pb.PbFieldType.O3)
-    ..hasRequiredFields = false;
-
+  factory Size() => create();
   Size._() : super();
-  factory Size({
-    $core.int? inches,
-  }) {
-    final _result = create();
-    if (inches != null) {
-      _result.inches = inches;
-    }
-    return _result;
-  }
   factory Size.fromBuffer($core.List<$core.int> i,
           [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) =>
       create()..mergeFromBuffer(i, r);
   factory Size.fromJson($core.String i,
           [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) =>
       create()..mergeFromJson(i, r);
+
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(
+      _omitMessageNames ? '' : 'Size',
+      package: const $pb.PackageName(_omitMessageNames ? '' : 'protos'),
+      createEmptyInstance: create)
+    ..a<$core.int>(1, _omitFieldNames ? '' : 'inches', $pb.PbFieldType.O3)
+    ..hasRequiredFields = false;
+
   @$core.Deprecated('Using this can add significant overhead to your binary. '
       'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
       'Will be removed in next major version')
@@ -54,9 +41,10 @@ class Size extends $pb.GeneratedMessage {
       'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
       'Will be removed in next major version')
   Size copyWith(void Function(Size) updates) =>
-      super.copyWith((message) => updates(message as Size))
-          as Size; // ignore: deprecated_member_use
+      super.copyWith((message) => updates(message as Size)) as Size;
+
   $pb.BuilderInfo get info_ => _i;
+
   @$core.pragma('dart2js:noInline')
   static Size create() => Size._();
   Size createEmptyInstance() => create();
@@ -80,57 +68,24 @@ class Size extends $pb.GeneratedMessage {
 }
 
 class Hat extends $pb.GeneratedMessage {
-  static final $pb.BuilderInfo _i = $pb.BuilderInfo(
-      const $core.bool.fromEnvironment('protobuf.omit_message_names')
-          ? ''
-          : 'Hat',
-      package: const $pb.PackageName(
-          const $core.bool.fromEnvironment('protobuf.omit_message_names')
-              ? ''
-              : 'protos'),
-      createEmptyInstance: create)
-    ..a<$core.int>(
-        1,
-        const $core.bool.fromEnvironment('protobuf.omit_field_names')
-            ? ''
-            : 'inches',
-        $pb.PbFieldType.O3)
-    ..aOS(
-        2,
-        const $core.bool.fromEnvironment('protobuf.omit_field_names')
-            ? ''
-            : 'color')
-    ..aOS(
-        3,
-        const $core.bool.fromEnvironment('protobuf.omit_field_names')
-            ? ''
-            : 'name')
-    ..hasRequiredFields = false;
-
+  factory Hat() => create();
   Hat._() : super();
-  factory Hat({
-    $core.int? inches,
-    $core.String? color,
-    $core.String? name,
-  }) {
-    final _result = create();
-    if (inches != null) {
-      _result.inches = inches;
-    }
-    if (color != null) {
-      _result.color = color;
-    }
-    if (name != null) {
-      _result.name = name;
-    }
-    return _result;
-  }
   factory Hat.fromBuffer($core.List<$core.int> i,
           [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) =>
       create()..mergeFromBuffer(i, r);
   factory Hat.fromJson($core.String i,
           [$pb.ExtensionRegistry r = $pb.ExtensionRegistry.EMPTY]) =>
       create()..mergeFromJson(i, r);
+
+  static final $pb.BuilderInfo _i = $pb.BuilderInfo(
+      _omitMessageNames ? '' : 'Hat',
+      package: const $pb.PackageName(_omitMessageNames ? '' : 'protos'),
+      createEmptyInstance: create)
+    ..a<$core.int>(1, _omitFieldNames ? '' : 'inches', $pb.PbFieldType.O3)
+    ..aOS(2, _omitFieldNames ? '' : 'color')
+    ..aOS(3, _omitFieldNames ? '' : 'name')
+    ..hasRequiredFields = false;
+
   @$core.Deprecated('Using this can add significant overhead to your binary. '
       'Use [GeneratedMessageGenericExtensions.deepCopy] instead. '
       'Will be removed in next major version')
@@ -139,9 +94,10 @@ class Hat extends $pb.GeneratedMessage {
       'Use [GeneratedMessageGenericExtensions.rebuild] instead. '
       'Will be removed in next major version')
   Hat copyWith(void Function(Hat) updates) =>
-      super.copyWith((message) => updates(message as Hat))
-          as Hat; // ignore: deprecated_member_use
+      super.copyWith((message) => updates(message as Hat)) as Hat;
+
   $pb.BuilderInfo get info_ => _i;
+
   @$core.pragma('dart2js:noInline')
   static Hat create() => Hat._();
   Hat createEmptyInstance() => create();
@@ -192,16 +148,14 @@ class HaberdasherApi {
   $pb.RpcClient _client;
   HaberdasherApi(this._client);
 
-  $async.Future<Hat> makeHat($pb.ClientContext? ctx, Size request) {
-    var emptyResponse = Hat();
-    return _client.invoke<Hat>(
-        ctx, 'Haberdasher', 'MakeHat', request, emptyResponse);
-  }
-
+  $async.Future<Hat> makeHat($pb.ClientContext? ctx, Size request) =>
+      _client.invoke<Hat>(ctx, 'Haberdasher', 'MakeHat', request, Hat());
   $async.Future<$0.Suit> makeSuit(
-      $pb.ClientContext? ctx, $0.SuitSizeReq request) {
-    var emptyResponse = $0.Suit();
-    return _client.invoke<$0.Suit>(
-        ctx, 'Haberdasher', 'MakeSuit', request, emptyResponse);
-  }
+          $pb.ClientContext? ctx, $0.SuitSizeReq request) =>
+      _client.invoke<$0.Suit>(
+          ctx, 'Haberdasher', 'MakeSuit', request, $0.Suit());
 }
+
+const _omitFieldNames = $core.bool.fromEnvironment('protobuf.omit_field_names');
+const _omitMessageNames =
+    $core.bool.fromEnvironment('protobuf.omit_message_names');
--- example/client/lib/protos/suit.pbenum.dart
+++ example/client/lib/protos/suit.pbenum.dart
@@ -1,23 +1,23 @@
-///
+//
 //  Generated code. Do not modify.
 //  source: suit.proto
 //
 // @dart = 2.12
-// ignore_for_file: annotate_overrides,camel_case_types,constant_identifier_names,directives_ordering,library_prefixes,non_constant_identifier_names,prefer_final_fields,return_of_invalid_type,unnecessary_const,unnecessary_import,unnecessary_this,unused_import,unused_shown_name
 
-// ignore_for_file: UNDEFINED_SHOWN_NAME
+// ignore_for_file: annotate_overrides, camel_case_types
+// ignore_for_file: constant_identifier_names, library_prefixes
+// ignore_for_file: non_constant_identifier_names, prefer_final_fields
+// ignore_for_file: unnecessary_import, unnecessary_this, unused_import
+
 import 'dart:core' as $core;
+
 import 'package:protobuf/protobuf.dart' as $pb;
 
 class SuitSize extends $pb.ProtobufEnum {
-  static const SuitSize SM = SuitSize._(0,
-      const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'SM');
-  static const SuitSize MD = SuitSize._(1,
-      const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'MD');
-  static const SuitSize LG = SuitSize._(2,
-      const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'LG');
-  static const SuitSize XL = SuitSize._(3,
-      const $core.bool.fromEnvironment('protobuf.omit_enum_names') ? '' : 'XL');
+  static const SuitSize SM = SuitSize._(0, _omitEnumNames ? '' : 'SM');
+  static const SuitSize MD = SuitSize._(1, _omitEnumNames ? '' : 'MD');
+  static const SuitSize LG = SuitSize._(2, _omitEnumNames ? '' : 'LG');
+  static const SuitSize XL = SuitSize._(3, _omitEnumNames ? '' : 'XL');
 
   static const $core.List<SuitSize> values = <SuitSize>[
     SM,
@@ -32,3 +32,5 @@ class SuitSize extends $pb.ProtobufEnum {
 
   const SuitSize._($core.int v, $core.String n) : super(v, n);
 }
+
+const _omitEnumNames = $core.bool.fromEnvironment('protobuf.omit_enum_names');
